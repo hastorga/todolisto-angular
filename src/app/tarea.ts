5b339fdae35a3cc10d65189ee875c1359c078da6
Héctor <hm.astorga@gmail.com>
2018-08-03 00:54:38 -0400

    fix: nuevos campos al constructor de tarea

--- src/app/tarea.ts
+++ src/app/tarea.ts
@@ -19,14 +19,16 @@ export class Tarea {
     estado: EstadoTarea;
     fecha_inicio: Date;
     fecha_termino: Date;
+    nombre_estado: string;
 
-    constructor(id, titulo, descripcion, fecha_inicio, fecha_termino, estado = EstadoTarea.Creada) {
+    constructor(id, titulo, descripcion, fecha_inicio, fecha_termino, estado = EstadoTarea.Creada, nombre_estado) {
         this.id = id;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.estado = estado;
         this.fecha_inicio = fecha_inicio;
         this.fecha_termino = fecha_termino;
+        this.nombre_estado = nombre_estado;
     }
 
     toString() {
